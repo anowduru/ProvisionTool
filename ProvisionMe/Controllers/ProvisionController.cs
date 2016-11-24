7042b8d99ce1df9827182dea24d1607b6e5a51b0
anowduru <avinash.nowduru@gmail.com>
2016-11-24 16:52:21 +0530

    Checked in fixing Certificate issue

--- ProvisionMe/Controllers/ProvisionController.cs
+++ ProvisionMe/Controllers/ProvisionController.cs
@@ -1,8 +1,10 @@
 ﻿using ProvisionMe.Models;
 using System;
 using System.Configuration;
+using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
+using System.Net.Security;
 using System.Web.Http;
 using System.Web.Http.Results;
 
@@ -13,6 +15,7 @@ namespace ProvisionMe.Controllers
         // POST api/<controller>            
         public JsonResult<ProvisionResponse> Post(PermissionRequest data)
         {
+            ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
             using (HttpClient client = new HttpClient())
             {
                 client.BaseAddress = new Uri(ConfigurationManager.AppSettings.Get("serviceUrl"));
